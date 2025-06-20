c5ad2227e36abddd6519d1985fdbf67ad6aaea6a
onuraltintas2045 <onur.altintas2045@gmail.com>
2025-06-21 02:22:46 +0300

    GCD Test

--- SwiftExamp.playground/Contents.swift
+++ SwiftExamp.playground/Contents.swift
@@ -1,3 +1,47 @@
 import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+var numbers = [1,7,13,4,5]
+
+print("sorted nums: \(numbers.sorted())")
+print("sorted nums: \(numbers.sorted(by: >))")
+
+testSerialQueueDeadlock()
+testGlobalQueueDeadlock()
+testMainQueueDeadlock()
+
+//Zaten Main threadde olduğumuz için deadlock oluşur
+DispatchQueue.main.sync {
+    print("deadlock")
+}
+
+func testSerialQueueDeadlock() {
+    let serialQueue = DispatchQueue(label: "serialQueue")
+
+    serialQueue.async {
+        print("serial async 1")
+        serialQueue.sync {
+            print("serial sync 2")
+        }
+    }
+}
+
+func testGlobalQueueDeadlock() {
+    DispatchQueue.global().async {
+        print("global async 1")
+        DispatchQueue.global().sync {
+            print("global sync 2")
+        }
+    }
+}
+
+func testMainQueueDeadlock() {
+    DispatchQueue.main.async {
+        print("main async 1")
+        DispatchQueue.main.sync {
+            print("main sync 2")
+        }
+    }
+}
+
+
