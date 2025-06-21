bfebacb0a20a557432482bcc2f501edc8fd7d5ed
onuraltintas2045 <onur.altintas2045@gmail.com>
2025-06-21 23:07:33 +0300

    Fibonacci completed

--- SwiftExamp.playground/Contents.swift
+++ SwiftExamp.playground/Contents.swift
@@ -44,8 +44,6 @@ func testMainQueueDeadlock() {
         }
     }
 }
-*/
-
 
 isPolindrome("ayak")
 
@@ -55,3 +53,14 @@ func isPolindrome(_ input: String) -> Bool {
     return lowerCasedInput == String(lowerCasedInput.reversed())
 }
 
+*/
+
+fibonacci(6)
+
+func fibonacci(_ n: Int) -> Int {
+    if n <= 1 {
+        return n
+    }
+    
+    return fibonacci(n - 1) + fibonacci(n - 2)
+}
